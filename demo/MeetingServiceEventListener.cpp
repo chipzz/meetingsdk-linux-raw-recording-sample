cd1137cc69df98db264906c019f8e2c79b32e26b
Jan Van Buggenhout <chipzz@chipzz.be>
2024-12-11 19:34:01 +0100

    Add onMeetingFullToWatchLiveStream method to MeetingServiceEventListener

--- demo/MeetingServiceEventListener.cpp
+++ demo/MeetingServiceEventListener.cpp
@@ -80,3 +80,8 @@ void MeetingServiceEventListener::onSuspendParticipantsActivities()
  void MeetingServiceEventListener::onMeetingTopicChanged(const zchar_t* sTopic)
  {
  }
+
+void MeetingServiceEventListener::onMeetingFullToWatchLiveStream(const zchar_t* sLiveStreamUrl)
+{
+
+}
