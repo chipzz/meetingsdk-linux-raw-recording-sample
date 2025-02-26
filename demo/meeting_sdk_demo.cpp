830a8c7d4bb2223b5daf7d7a498590b5d417765a
Jan Van Buggenhout <chipzz@chipzz.be>
2025-02-26 14:54:18 +0100

    useCamera

--- demo/meeting_sdk_demo.cpp
+++ demo/meeting_sdk_demo.cpp
@@ -306,6 +306,8 @@ void turnOffSendVideoandAudio() {
 }
 
 
+bool useCamera;
+
 //callback when the SDK is inmeeting
 void onInMeeting() {
 
@@ -322,6 +324,8 @@ void onInMeeting() {
 
 	//first attempt to start raw recording  / sending, upon successfully joined and achieved "in-meeting" state.
 	CheckAndStartRawRecording(GetVideoRawData, GetAudioRawData);
+	SendVideoRawData = !useCamera;
+	SendAudioRawData = !useCamera;
 	CheckAndStartRawSending(SendVideoRawData, SendAudioRawData);
 
 }
@@ -893,6 +897,7 @@ void initAppSettings()
 
 int main(int argc, char* argv[])
 {
+	useCamera = (argc > 1);
 	ReadTEXTSettings();
 	InitMeetingSDK();
 	AuthMeetingSDK();
